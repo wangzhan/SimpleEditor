fe1769a7b490f76dee554f136560ea43a65ed68e
wangzhan <wangzhan.1985@gmail.com>
2013-12-24 17:44:08 +0800

    add the files

---
+++ main.js
@@ -0,0 +1,24 @@
+﻿define(function (require) {
+    var Editor = require('./scripts/view/editorView');
+
+    var editor = new Editor({
+        textarea: '#main',
+
+        panelItems: [
+            require('./scripts/toolbar/bold'),
+            require('./scripts/toolbar/toggle')
+            
+        ],
+
+        commands: [
+            require('./scripts/command/nativeCommand')
+        ],
+    });
+
+    editor.render();
+
+    // 暴露
+    if (!window.editor) {
+        window.editor = editor;
+    }
+});
